d6b6409f626f53aa35e8d2edab9ca05068bf3895
martinvaud-create <martin.vaud@le-campus-numerique.fr>
2026-03-25 11:48:28 +0100

    Update Magasin.java

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,51 +9,54 @@ class Magasin {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
             if (!items[i].name.equals("Comté")
                     && !items[i].name.equals("Pass VIP Concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Kryptonite")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+
+                if (items[i].quality > 0 && !items[i].name.equals("Kryptonite")) {
+                    items[i].quality--;
                 }
+
             } else {
+
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality++;
 
                     if (items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality++;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                            items[i].quality++;
                         }
                     }
                 }
             }
 
             if (!items[i].name.equals("Kryptonite")) {
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
             }
 
             if (items[i].sellIn < 0) {
+
                 if (!items[i].name.equals("Comté")) {
+
                     if (!items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Kryptonite")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+
+                        if (items[i].quality > 0 && !items[i].name.equals("Kryptonite")) {
+                            items[i].quality--;
                         }
+
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
+
                 } else {
+
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        items[i].quality++;
                     }
                 }
             }
